c15babbb0eaa89f20f80b87a17416aad14472253
ryantran <113406848+rryantran@users.noreply.github.com>
2024-08-26 17:13:40 -0500

    set up supabase and test connection

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,22 @@
+import supabase from "./supabase";
+
+async function testConnection() {
+  try {
+    const { data, error } = await supabase.from("test").select("*");
+
+    if (error) {
+      console.error("Error retrieving data: ", error);
+      return;
+    }
+
+    console.log("Data retrieved: ", data);
+  } catch (err) {
+    console.error("Error: ", err);
+  }
+}
 const App = () => {
+  testConnection();
+
   return <></>;
 };
 
